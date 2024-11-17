068281704b022c3dec6ca76e1f8edba9ea638b68
Dhanapal30-gif <dhanapal05v@gmail.com>
2024-11-17 23:04:04 +0530

    cretae end point methods

--- src/main/java/orkaTraks/Controller/ProjectsController.java
+++ src/main/java/orkaTraks/Controller/ProjectsController.java
@@ -1,11 +1,13 @@
 package orkaTraks.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import orkaTraks.Entity.EmployeeDetails;
 import orkaTraks.Entity.ProjectManagement;
 import orkaTraks.Service.ProjectsService;
 
@@ -19,5 +21,15 @@ public class ProjectsController {
     public ResponseEntity<List<ProjectManagement>>getProject(){
         List<ProjectManagement>getProject=projectsService.getProject();
         return ResponseEntity.ok(getProject);
+    }
+
+    @GetMapping("/api/projectController/getProjectDetail")
+    public ResponseEntity<List<ProjectManagement>> getProjectDeatil(){
+        List <ProjectManagement> getEmployee=projectsService.getProjectDetail();
+
+        if (getEmployee.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(getEmployee, HttpStatus.OK);
     }
 }
