62fdd5e2dab01f333b8859339732be57311dc68d
Dhanapal30-gif <dhanapal05v@gmail.com>
2024-11-23 04:39:24 +0530

    update code

---
+++ src/main/java/orkaTraks/Controller/Accounts.java
@@ -0,0 +1,45 @@
+package orkaTraks.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import orkaTraks.Entity.Account;
+import orkaTraks.Entity.ProjectManagement;
+import orkaTraks.Entity.Task;
+import orkaTraks.Service.Accounts_servie;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+
+public class Accounts {
+    @Autowired
+    private Accounts_servie accoutsService;
+
+    @PostMapping("/api/accountsController/accountManagement")
+    public ResponseEntity<List<Account>> saveAccountDetail(@RequestBody List<Account> accounts) {
+        try {
+            List<Account> savedtask = accoutsService.saveTask(accounts);
+            if (savedtask == null || savedtask.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedtask); // Return the saved tasks
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null); // Return 500 on error
+
+        }
+
+    }
+
+
+    @GetMapping("/api/Account/getallAccountdeatil")
+    public ResponseEntity<List<Account>> getProject(){
+        List<Account> project = accoutsService.getAccount();
+        return ResponseEntity.ok(project);
+    }
+}
---
+++ src/main/java/orkaTraks/Service/Accounts_servie.java
@@ -0,0 +1,29 @@
+package orkaTraks.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import orkaTraks.Entity.Account;
+import orkaTraks.Entity.Task;
+import orkaTraks.Repo.Accounts_Repo;
+
+import java.util.List;
+
+@Service
+public class Accounts_servie {
+    @Autowired
+    private Accounts_Repo accountsRepo;
+
+
+    public List<Account> saveTask(List<Account> accounts) {
+        return accountsRepo.saveAll(accounts);
+
+
+    }
+
+
+
+    public List<Account> getAccount() {
+        return accountsRepo.findAll();
+    }
+}
