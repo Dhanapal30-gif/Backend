357d6bce9067883a29e5809ea1b37be3fd060ad7
Dhanapal30-gif <dhanapal05v@gmail.com>
2024-11-27 10:39:19 +0530

    Add delete button

--- src/main/java/orkaTraks/Controller/Accounts.java
+++ src/main/java/orkaTraks/Controller/Accounts.java
@@ -5,11 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import orkaTraks.Entity.Account;
-import orkaTraks.Entity.ProjectManagement;
-import orkaTraks.Entity.Task;
 import orkaTraks.Service.Accounts_servie;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
--- src/main/java/orkaTraks/Service/Accounts_servie.java
+++ src/main/java/orkaTraks/Service/Accounts_servie.java
@@ -21,8 +21,6 @@ public class Accounts_servie {
 
     }
 
-
-
     public List<Account> getAccount() {
         return accountsRepo.findAll();
     }
